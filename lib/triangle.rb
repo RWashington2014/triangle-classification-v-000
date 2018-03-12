f1b6608ca97a14ddefa20df7a493d0f4dfabfff4
Robert W <robwash@gmail.com>
2018-03-12 13:04:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,13 +8,21 @@ class Triangle
     end
 
     def kind
-      if self.side_1 == self.side_2 && self.side_1 == self.side_3
-        :equilateral
+      if !self.is_triangle?(side_1, side_2, side_3)
+        raise TriangleError
       elsif self.side_1 != self.side_2 && self.side_1 != self.side_3 && self.side_2 != self.side_3
         :scalene
+      elsif self.side_1 == self.side_2 && self.side_1 == self.side_3
+        :equilateral
       else
         :isosceles
       end
+    end
+    
+    def is_triangle?(a,b,c)
+      sorted = [a,b,c].sort
+      greatest_side = sorted.pop
+      greatest_side < sorted.sum
     end
 end # => End Triangle class
 
